caeef54116b00b436a599dae4980276b84c5b994
Artem <artekormio@gmail.com>
2022-10-16 22:22:53 +0200

    json catalog send me added

--- internal/app/commands/exit.go
+++ internal/app/commands/exit.go
@@ -1,17 +1,27 @@
 package commands
 
 import (
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/kormiltsev/tbot-welly/internal/product"
 
-	"log"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
-
-var mineID = int64(278284468)
 
 // func Menu1(bot *tgbotapi.BotAPI, update tgbotapi.Update, Users *Ids64) {
 func SaveAndQuit(bot *tgbotapi.BotAPI) {
+	godotenv.Load()
+	mineID := os.Getenv("MYID")
+	if mineID == "" {
+		return
+	}
+	chatid64, err := strconv.ParseInt(mineID, 10, 64)
+	if err != nil {
+		log.Panic(err)
+	}
 	//t := "@Collectionist_bot was stoped\n"
 	catalogjson := product.CatalogAdres()
 	//t := "Catalog-" + strconv.FormatInt(time.Now().Unix(), 10) + ".json"
@@ -23,9 +33,9 @@ func SaveAndQuit(bot *tgbotapi.BotAPI) {
 	//data, _ := ioutil.ReadFile(catalogjson)
 	listMediaInput = append(listMediaInput, tgbotapi.NewInputMediaDocument(tgbotapi.FilePath(catalogjson)))
 
-	msg := tgbotapi.NewMediaGroup(mineID, listMediaInput)
+	msg := tgbotapi.NewMediaGroup(chatid64, listMediaInput)
 
-	_, err := bot.Send(msg)
+	_, err = bot.Send(msg)
 	if err != nil {
 		log.Panic(err)
 	}
